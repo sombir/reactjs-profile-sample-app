842b198bfbfd689dab33bc418d4d68a08855cfbc
Sombir Singh <sombir.singh@aricent.com>
2017-08-29 14:32:21 +0530

    first commit

---
+++ app/components/section/footer.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const React = require('react');
+
+const ResumePropTypes = require('../../prop_types/resume');
+const SocialMedia = require('../social_media');
+
+const Footer = React.createClass({
+    propTypes: {
+        content: ResumePropTypes.basics
+    },
+
+    render: function () {
+        return (
+            <footer>
+                <div className='row'>
+                    <div className='twelve columns'>
+                        <SocialMedia ulClass='social-links' profiles={this.props.content.profiles}/>
+                        <ul className='copyright'>
+                            <li>
+                                This site is developed in React.js by&nbsp;
+                                <a
+                                    href='https://sombirsingh.com'
+                                    title='Sombir Singh'
+                                    target='_blank'
+                                    rel='noopener noreferrer'>
+                                    Sombir Singh
+                                </a> 
+                            </li>
+                        </ul>
+                    </div>
+                    <div id='go-top'>
+                        <a className='smoothscroll' title='Back to Top' href='#home'>
+                            <i className='icon-up-open'/>
+                        </a>
+                    </div>
+                </div>
+            </footer>
+        );
+    }
+});
+
+module.exports = Footer;
